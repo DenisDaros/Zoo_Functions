d8720307767eb45eacfb5775c44ebfd16cfc4fd4
Denis <denisv.daros@gmail.com>
2022-05-26 19:24:19 -0300

    sexto e sétimo concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,14 +1,14 @@
 const data = require('../data/zoo_data');
 
-const { employees } = data
+const { employees } = data;
 function isManager(id) {
-  return employees.some(index => index.managers.includes(id))
+  return employees.some((index) => index.managers.includes(id));
 }
-console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
+
 function getRelatedEmployees(managerId) {
 //  if (isManager(managerId) === true) {
 
- }
+}
 
 
 module.exports = { isManager, getRelatedEmployees };
