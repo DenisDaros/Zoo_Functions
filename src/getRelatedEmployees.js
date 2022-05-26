88437e26e35413c725ff8b1c652959d8e5d48026
Denis <denisv.daros@gmail.com>
2022-05-26 18:51:46 -0300

    sexto e sétimo concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data
 function isManager(id) {
-  // seu código aqui
+  return employees.some(index => index.managers.includes(id))
 }
-
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
 function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+//  if (isManager(managerId) === true) {
+
+ }
+
 
 module.exports = { isManager, getRelatedEmployees };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,28 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Para o argumento count deve retornar o número inteiro 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Para o argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).toEqual(expect.not.arrayContaining(['Monday']));
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Para o argumento 2 deve retornar uma string "Parâmetro inválido, é necessário uma string"', () => {
+    expect(handlerElephants(2)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+});
