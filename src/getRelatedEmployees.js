813bceeb2ce04f529bd2a3ed9c290283a41cb101
Denis <denisv.daros@gmail.com>
2022-05-27 15:53:25 -0300

    requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -6,8 +6,12 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-//  if (isManager(managerId) === true) {
-
+  if (isManager(managerId) === true) {
+    const separandoColaboradores = employees.filter((aux) => aux.managers.includes(managerId));
+    return separandoColaboradores.map((i) => `${i.firstName} ${i.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
