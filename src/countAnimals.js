6aa393fd05782c4d9ca4ce5c69eb7c921f5b9bba
Denis <denisv.daros@gmail.com>
2022-05-27 23:57:41 -0300

    requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function listaAnimal() {
+  return species.reduce((acc, aux) => {
+    acc[aux.name] = aux.residents.length;
+    return acc;
+  }, {});
 }
 
+function retornarAnimaisFemale(specie, sex) {
+  return species.reduce((acc, aux) => {
+    if (specie === aux.name) {
+      return aux.residents.filter((index) => index.sex === sex).length;
+    }
+    return acc;
+  }, 0);
+}
+function countAnimals(animal) {
+  if (!animal) {
+    return listaAnimal();
+  }
+  const { specie, sex } = animal;
+  if (specie && sex) {
+    return retornarAnimaisFemale(specie, sex);
+  }
+  return listaAnimal()[specie];
+}
+console.log(countAnimals({ specie: 'lions', sex: 'male' }));
 module.exports = countAnimals;
