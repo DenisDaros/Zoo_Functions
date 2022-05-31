4c12b817afaae26aa00cdf159e876731958f944d
Denis <denisv.daros@gmail.com>
2022-05-31 00:13:34 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const procurandoPorId = employees.find((index) => index.id === id);
+  const procurandoPorAnimal = procurandoPorId.responsibleFor[0];
+  const separandoBlocoDoAnimal = species.find((i) => i.id === procurandoPorAnimal);
+  const separandoResidentes = separandoBlocoDoAnimal.residents;
+  const animalMaisVelho = separandoResidentes.reduce((acc, curr) => {
+    if (acc.age > curr.age) {
+      return acc;
+    }
+    return curr;
+  });
+  return Object.values(animalMaisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
