5cfa7bff4b52c92f8744514e20fbef5d71e0869c
Denis <denisv.daros@gmail.com>
2022-05-31 22:53:57 -0300

    requisito 9 concluido

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const { species, hours } = data;
 
+function procurandoOsDias(animal) {
+  return species.find((index) => index.name === animal).availability;
+}
+// console.log(procurandoOsDias('lions'));
+
+function objetoCria(dia) {
+  if (dia === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  const { open, close } = hours[dia];
+  const objetoCriado = {};
+  objetoCriado[dia] = {
+    officeHour: `Open from ${open}am until ${close}pm`,
+    exhibition: species.filter((index) => index.availability
+      .includes(dia))
+      .map((i) => i.name),
+  };
+
+  return objetoCriado;
+}
+console.log(species.name);
+function getSchedule(scheduleTarget) {
+  if (species.find((index) => index.name.includes(scheduleTarget))) {
+    return procurandoOsDias(scheduleTarget);
+  }
+  if (Object.keys(hours).includes(scheduleTarget)) {
+    return objetoCria(scheduleTarget);
+  }
+  return Object.keys(hours).reduce((acc, curr) => ({ ...acc, ...objetoCria(curr) }), {});
 }
 
 module.exports = getSchedule;
